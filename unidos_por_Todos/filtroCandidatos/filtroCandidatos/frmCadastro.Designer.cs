4c989a963d4fb53e8ddd15d0aef684319cc3d9a5
bertoloteDev <bertolotematheus@gmail.com>
2023-07-26 22:05:41 -0300

    Mudei o design, programei a idade necessária de cadastro e adicionei para escolher o gênero

--- unidos_por_Todos/filtroCandidatos/filtroCandidatos/frmCadastro.Designer.cs
+++ unidos_por_Todos/filtroCandidatos/filtroCandidatos/frmCadastro.Designer.cs
@@ -45,13 +45,18 @@ namespace filtroCandidatos
             this.Password = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.Register = new System.Windows.Forms.Button();
+            this.Men = new System.Windows.Forms.RadioButton();
+            this.Woman = new System.Windows.Forms.RadioButton();
+            this.Gender = new System.Windows.Forms.GroupBox();
+            this.Sex = new System.Windows.Forms.Label();
+            this.Gender.SuspendLayout();
             this.SuspendLayout();
             // 
             // nome
             // 
             this.nome.AutoSize = true;
             this.nome.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nome.Location = new System.Drawing.Point(144, 128);
+            this.nome.Location = new System.Drawing.Point(37, 133);
             this.nome.Name = "nome";
             this.nome.Size = new System.Drawing.Size(54, 18);
             this.nome.TabIndex = 0;
@@ -62,7 +67,7 @@ namespace filtroCandidatos
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(131, 158);
+            this.label2.Location = new System.Drawing.Point(24, 163);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(94, 18);
             this.label2.TabIndex = 1;
@@ -73,7 +78,7 @@ namespace filtroCandidatos
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(72, 194);
+            this.label3.Location = new System.Drawing.Point(12, 201);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(155, 18);
             this.label3.TabIndex = 2;
@@ -84,7 +89,7 @@ namespace filtroCandidatos
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(422, 128);
+            this.label4.Location = new System.Drawing.Point(541, 111);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(45, 18);
             this.label4.TabIndex = 3;
@@ -94,7 +99,7 @@ namespace filtroCandidatos
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(411, 158);
+            this.label5.Location = new System.Drawing.Point(530, 141);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(70, 18);
             this.label5.TabIndex = 4;
@@ -104,7 +109,7 @@ namespace filtroCandidatos
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(417, 191);
+            this.label6.Location = new System.Drawing.Point(536, 174);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(52, 18);
             this.label6.TabIndex = 5;
@@ -114,7 +119,7 @@ namespace filtroCandidatos
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(415, 220);
+            this.label1.Location = new System.Drawing.Point(534, 203);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 18);
             this.label1.TabIndex = 6;
@@ -124,62 +129,62 @@ namespace filtroCandidatos
             // Birth
             // 
             this.Birth.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Birth.Location = new System.Drawing.Point(230, 189);
+            this.Birth.Location = new System.Drawing.Point(173, 195);
             this.Birth.Name = "Birth";
-            this.Birth.Size = new System.Drawing.Size(114, 26);
+            this.Birth.Size = new System.Drawing.Size(305, 26);
             this.Birth.TabIndex = 7;
             this.Birth.ValueChanged += new System.EventHandler(this.Birth_ValueChanged);
             // 
             // txtNome
             // 
             this.txtNome.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNome.Location = new System.Drawing.Point(232, 125);
+            this.txtNome.Location = new System.Drawing.Point(122, 125);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(100, 26);
+            this.txtNome.Size = new System.Drawing.Size(149, 26);
             this.txtNome.TabIndex = 8;
             this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
             // 
             // LastName
             // 
             this.LastName.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LastName.Location = new System.Drawing.Point(232, 155);
+            this.LastName.Location = new System.Drawing.Point(122, 160);
             this.LastName.Name = "LastName";
-            this.LastName.Size = new System.Drawing.Size(100, 26);
+            this.LastName.Size = new System.Drawing.Size(149, 26);
             this.LastName.TabIndex = 9;
             this.LastName.TextChanged += new System.EventHandler(this.LastName_TextChanged);
             // 
             // Email
             // 
             this.Email.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Email.Location = new System.Drawing.Point(491, 186);
+            this.Email.Location = new System.Drawing.Point(611, 169);
             this.Email.Name = "Email";
-            this.Email.Size = new System.Drawing.Size(100, 26);
+            this.Email.Size = new System.Drawing.Size(190, 26);
             this.Email.TabIndex = 10;
             // 
             // PhoneNumber
             // 
             this.PhoneNumber.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PhoneNumber.Location = new System.Drawing.Point(492, 155);
+            this.PhoneNumber.Location = new System.Drawing.Point(611, 138);
             this.PhoneNumber.Name = "PhoneNumber";
-            this.PhoneNumber.Size = new System.Drawing.Size(100, 26);
+            this.PhoneNumber.Size = new System.Drawing.Size(190, 26);
             this.PhoneNumber.TabIndex = 11;
             // 
             // cpf
             // 
             this.cpf.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cpf.Location = new System.Drawing.Point(492, 125);
+            this.cpf.Location = new System.Drawing.Point(611, 108);
             this.cpf.Name = "cpf";
-            this.cpf.Size = new System.Drawing.Size(100, 26);
+            this.cpf.Size = new System.Drawing.Size(190, 26);
             this.cpf.TabIndex = 12;
             this.cpf.Text = " ";
             // 
             // Password
             // 
             this.Password.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Password.Location = new System.Drawing.Point(492, 217);
+            this.Password.Location = new System.Drawing.Point(611, 200);
             this.Password.Name = "Password";
             this.Password.PasswordChar = '*';
-            this.Password.Size = new System.Drawing.Size(100, 26);
+            this.Password.Size = new System.Drawing.Size(190, 26);
             this.Password.TabIndex = 13;
             this.Password.TextChanged += new System.EventHandler(this.Password_TextChanged);
             // 
@@ -196,7 +201,7 @@ namespace filtroCandidatos
             // Register
             // 
             this.Register.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Register.Location = new System.Drawing.Point(302, 295);
+            this.Register.Location = new System.Drawing.Point(394, 361);
             this.Register.Name = "Register";
             this.Register.Size = new System.Drawing.Size(135, 35);
             this.Register.TabIndex = 15;
@@ -204,11 +209,55 @@ namespace filtroCandidatos
             this.Register.UseVisualStyleBackColor = true;
             this.Register.Click += new System.EventHandler(this.button1_Click);
             // 
+            // Men
+            // 
+            this.Men.AutoSize = true;
+            this.Men.Location = new System.Drawing.Point(28, 42);
+            this.Men.Name = "Men";
+            this.Men.Size = new System.Drawing.Size(73, 17);
+            this.Men.TabIndex = 16;
+            this.Men.TabStop = true;
+            this.Men.Text = "Masculino";
+            this.Men.UseVisualStyleBackColor = true;
+            // 
+            // Woman
+            // 
+            this.Woman.AutoSize = true;
+            this.Woman.Location = new System.Drawing.Point(28, 19);
+            this.Woman.Name = "Woman";
+            this.Woman.Size = new System.Drawing.Size(67, 17);
+            this.Woman.TabIndex = 17;
+            this.Woman.TabStop = true;
+            this.Woman.Text = "Feminino";
+            this.Woman.UseVisualStyleBackColor = true;
+            // 
+            // Gender
+            // 
+            this.Gender.Controls.Add(this.Men);
+            this.Gender.Controls.Add(this.Woman);
+            this.Gender.Location = new System.Drawing.Point(80, 227);
+            this.Gender.Name = "Gender";
+            this.Gender.Size = new System.Drawing.Size(124, 76);
+            this.Gender.TabIndex = 18;
+            this.Gender.TabStop = false;
+            // 
+            // Sex
+            // 
+            this.Sex.AutoSize = true;
+            this.Sex.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sex.Location = new System.Drawing.Point(24, 237);
+            this.Sex.Name = "Sex";
+            this.Sex.Size = new System.Drawing.Size(48, 18);
+            this.Sex.TabIndex = 19;
+            this.Sex.Text = "Sexo:";
+            // 
             // frmCadastro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(925, 450);
+            this.Controls.Add(this.Sex);
+            this.Controls.Add(this.Gender);
             this.Controls.Add(this.Register);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.Password);
@@ -228,6 +277,8 @@ namespace filtroCandidatos
             this.Name = "frmCadastro";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.frmCadastro_Load);
+            this.Gender.ResumeLayout(false);
+            this.Gender.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -251,5 +302,9 @@ namespace filtroCandidatos
         private System.Windows.Forms.TextBox Password;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button Register;
+        private System.Windows.Forms.RadioButton Men;
+        private System.Windows.Forms.RadioButton Woman;
+        private System.Windows.Forms.GroupBox Gender;
+        private System.Windows.Forms.Label Sex;
     }
 }
--- unidos_por_Todos/filtroCandidatos/filtroCandidatos/frmCadastro.cs
+++ unidos_por_Todos/filtroCandidatos/filtroCandidatos/frmCadastro.cs
@@ -34,44 +34,54 @@ namespace filtroCandidatos
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string nome = txtNome.Text;
-            // Declarei a variável que é uma string e coloquei o "txtNome.Text" nessa variável
-            string lastname = LastName.Text;
-            // Declarei a variável que é uma string e coloquei o "LastName.Text" nessa variável
-            string DatadeNascimento = Birth.Value.ToString("yyyy-MM-dd");
-            // Declarei a variável que é uma string e coloquei o "Birth" nessa variável e como ela é um valor numérico ela acompanha o .Value e depois usei o ToString e depois eu coloquei o formato que eu quero que é "yyyy" que significa o ano em 4 digítos, o "MM" que significa o mês em 2 digítos e o "dd" que significa dias em 2 digítos
-            string CPF = cpf.Text;
-            // Declarei a variável que é uma string e coloquei o "cpf.Text" nessa variável
-            string telefone = PhoneNumber.Text;
-            // Declarei a variável que é uma string e coloquei o "PhoneNumber.Text" nessa variável
-            string email = Email.Text;
-            // Declarei a variável que é uma string e coloquei o "Email.Text" nessa variável
-            string senha = Password.Text;
-            // Declarei a variável que é uma string e coloquei o "Password.Text" nessa variável
-          
+            string nome = txtNome.Text; // Declarei a variável que é uma string e coloquei o "txtNome.Text" nessa variável
+            
+            string lastname = LastName.Text;// Declarei a variável que é uma string e coloquei o "LastName.Text" nessa variável
+            
+            string DatadeNascimento = Birth.Value.ToString("yyyy-MM-dd"); // Declarei a variável que é uma string e coloquei o "Birth" nessa variável e como ela é um valor numérico ela acompanha o .Value e depois usei o ToString e depois eu coloquei o formato que eu quero que é "yyyy" que significa o ano em 4 digítos, o "MM" que significa o mês em 2 digítos e o "dd" que significa dias em 2 digítos
+            
+            string CPF = cpf.Text;// Declarei a variável que é uma string e coloquei o "cpf.Text" nessa variável
+            
+            string telefone = PhoneNumber.Text;// Declarei a variável que é uma string e coloquei o "PhoneNumber.Text" nessa variável
+            
+            string email = Email.Text;// Declarei a variável que é uma string e coloquei o "Email.Text" nessa variável
+            
+            string senha = Password.Text;// Declarei a variável que é uma string e coloquei o "Password.Text" nessa variável
 
-            if (nome != "" && lastname != "" && DatadeNascimento != "" && CPF != "" && telefone != "" && email != "" && senha != "")
-                // Declarei um if para verificar se os campos de cadastros estão preenchidos
+            string opcao = "";
+            if (Woman.Checked)
             {
-                MessageBox.Show("Usuário Cadastrado com Sucesso");
-                // Se os campos estiverem preenchidos aparecerá um MessageBox escrito "Usuário Cadastrado com sucesso"
+                opcao = "Feminino";
             }
-            else 
-            // Se todos os campos não estiverem preenchidos irá aparecer um MessageBox escrito "Informações Inválidas" 
+            
+
+            DateTime today = DateTime.Now; // DateTime é um tipo de váriavel que marca o dia e a hora,  declarei a variável chamda "today", chamei uma classe nativa do C# ("DateTime") e peguei um atributo dela que retorna o dia de hoje("Now")
+
+            DateTime birthdate = Birth.Value; // Chamei a classe nativa do c# "DateTime", declarei a variável "birthdate" e atribui o valor da string "Birth.Value" dentro da variável
+
+            int age = (int)((today - birthdate).TotalDays / 365.25); // Declarei a variável "age" e especifiquei que a classe dela é inteira "int" e subtrai as variáveis "today" e "birthdate" multiplicando com o total dias do ano dividindo por 365.25
+
+            if (age < 18)// Verificar se o usuário tem 18 anos para efetuar o cadastro. SE Variável "age" for menor que 18 irá aparecer uma mensagem dizendo que o usuário não possui a idade necessária
             {
-              MessageBox.Show(" Informações Inválidas ");
-                // Se todos os campos não estiverem preenchidos irá aparecer um MessageBox escrito "Informações Inválidas" 
+                MessageBox.Show(" Não tem a idade necessária ");
+                return;
+               
             }
 
-            if (DatadeNascimento == "25-07-2005")
+            if (nome != "" && lastname != "" && DatadeNascimento != "" && CPF != "" && telefone != "" && email != "" && senha != "")// Declarei um if para verificar se os campos de cadastros estão preenchidos
+                                                                                                                                    
             {
+                MessageBox.Show("Usuário Cadastrado com Sucesso");// Se os campos estiverem preenchidos aparecerá um MessageBox escrito "Usuário Cadastrado com sucesso"
                 
             }
-             else
+            else // Se todos os campos não estiverem preenchidos irá aparecer um MessageBox escrito "Informações Inválidas"
+            
             {
-                MessageBox.Show("NÃO POSSUI A IDADE NECESSÁRIA");
+              MessageBox.Show(" Informações Inválidas ");  // Se todos os campos não estiverem preenchidos irá aparecer um MessageBox escrito "Informações Inválidas"
+                
             }
 
+           
 
         }
 
