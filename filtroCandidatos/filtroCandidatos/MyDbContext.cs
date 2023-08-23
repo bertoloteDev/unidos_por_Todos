6ed7f02278fcf63c544dfafb70874096c5536d9e
GABRIEL FRAZAO BRAZ <gabrielftot@gmail.com>
2023-08-22 22:03:22 -0300

    conexão com  banco de dados

---
+++ filtroCandidatos/filtroCandidatos/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace filtroCandidatos
+{
+    class MyDbContext:DbContext
+    {
+        public MyDbContext() : base("name=MyDbContext") { }
+    }
+}
