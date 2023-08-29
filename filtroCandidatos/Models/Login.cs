fdb781a6c46485d2e0e0356d614ffcff6ee14606
Grindamaral <ingridamaral0707@gmail.com>
2023-08-28 21:59:52 -0300

    inserir dados na tabela login

---
+++ filtroCandidatos/Models/Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace filtroCandidatos.Models
+{
+
+    public class  Login
+    {
+
+        [Column("id")]
+        public int id { get; set; }
+
+        [Column("email")]
+        public int email{ get; set; }
+
+        [Column("tipo_de_acesso")]
+        public string tipo_de_acesso { get; set; }
+
+        [Column("senha")]
+        public string senha { get; set; }
+
+        [Column("id_cadastro")]
+        public string id_cadastro { get; set; }
+
+    }
+}
