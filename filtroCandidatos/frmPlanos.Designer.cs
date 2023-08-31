c885a19010b5890c295960f51a575ceebd5a5fcc
GABRIEL FRAZAO BRAZ <gabrielftot@gmail.com>
2023-08-30 21:45:47 -0300

    fiz tela de planos de pagamentos

---
+++ filtroCandidatos/frmPlanos.Designer.cs
@@ -0,0 +1,293 @@
+﻿
+namespace filtroCandidatos
+{
+    partial class frmPlanos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPadrao = new System.Windows.Forms.Button();
+            this.btnBasico = new System.Windows.Forms.Button();
+            this.btnPremium = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.voltarATelaPrincipalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnBasico);
+            this.groupBox1.Controls.Add(this.btnPremium);
+            this.groupBox1.Controls.Add(this.btnPadrao);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(252, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(925, 516);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(212, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(490, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Escolha o melhor plano para você.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnPadrao
+            // 
+            this.btnPadrao.BackColor = System.Drawing.Color.White;
+            this.btnPadrao.Font = new System.Drawing.Font("Mongolian Baiti", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPadrao.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.btnPadrao.Location = new System.Drawing.Point(386, 152);
+            this.btnPadrao.Name = "btnPadrao";
+            this.btnPadrao.Size = new System.Drawing.Size(140, 100);
+            this.btnPadrao.TabIndex = 1;
+            this.btnPadrao.Text = "Padrão";
+            this.btnPadrao.UseVisualStyleBackColor = false;
+            this.btnPadrao.Click += new System.EventHandler(this.btnPadrao_Click);
+            // 
+            // btnBasico
+            // 
+            this.btnBasico.BackColor = System.Drawing.Color.White;
+            this.btnBasico.Font = new System.Drawing.Font("Mongolian Baiti", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBasico.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.btnBasico.Location = new System.Drawing.Point(166, 152);
+            this.btnBasico.Name = "btnBasico";
+            this.btnBasico.Size = new System.Drawing.Size(140, 100);
+            this.btnBasico.TabIndex = 1;
+            this.btnBasico.Text = "Básico";
+            this.btnBasico.UseVisualStyleBackColor = false;
+            this.btnBasico.Click += new System.EventHandler(this.btnBasico_Click);
+            // 
+            // btnPremium
+            // 
+            this.btnPremium.BackColor = System.Drawing.Color.White;
+            this.btnPremium.Font = new System.Drawing.Font("Mongolian Baiti", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPremium.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.btnPremium.Location = new System.Drawing.Point(600, 152);
+            this.btnPremium.Name = "btnPremium";
+            this.btnPremium.Size = new System.Drawing.Size(140, 100);
+            this.btnPremium.TabIndex = 1;
+            this.btnPremium.Text = "Premium";
+            this.btnPremium.UseVisualStyleBackColor = false;
+            this.btnPremium.Click += new System.EventHandler(this.btnPremium_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label2.Location = new System.Drawing.Point(187, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "R$: 0,00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label3.Location = new System.Drawing.Point(397, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "R$: 500,00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label4.Location = new System.Drawing.Point(605, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "R$: 3,000,00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(198, 341);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Dados de:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(418, 341);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 20);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Dados de:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(634, 341);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 20);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Dados de:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Location = new System.Drawing.Point(188, 382);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(98, 20);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Uma cidade.";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label9.Location = new System.Drawing.Point(416, 382);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(102, 20);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Uma Região.";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label10.Location = new System.Drawing.Point(624, 382);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(105, 20);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "Todo o Brasil.";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1428, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.BackColor = System.Drawing.Color.White;
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.voltarATelaPrincipalToolStripMenuItem});
+            this.menuToolStripMenuItem.Font = new System.Drawing.Font("Mongolian Baiti", 11.25F);
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // voltarATelaPrincipalToolStripMenuItem
+            // 
+            this.voltarATelaPrincipalToolStripMenuItem.Name = "voltarATelaPrincipalToolStripMenuItem";
+            this.voltarATelaPrincipalToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.voltarATelaPrincipalToolStripMenuItem.Text = "Voltar a tela principal";
+            this.voltarATelaPrincipalToolStripMenuItem.Click += new System.EventHandler(this.voltarATelaPrincipalToolStripMenuItem_Click);
+            // 
+            // frmPlanos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1428, 644);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmPlanos";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.frmPlanos_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBasico;
+        private System.Windows.Forms.Button btnPremium;
+        private System.Windows.Forms.Button btnPadrao;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem voltarATelaPrincipalToolStripMenuItem;
+    }
+}
---
+++ filtroCandidatos/frmPlanos.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace filtroCandidatos
+{
+    public partial class frmPlanos : Form
+    {
+        public frmPlanos()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPlanos_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            int x = (this.Width - groupBox1.Width) / 2;
+            int y = (this.Height - groupBox1.Height) / 2;
+            groupBox1.Location = new Point(x, y);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void voltarATelaPrincipalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form frmLogin = new frmLogin();
+            frmLogin.Show();
+        }
+
+        private void btnPadrao_Click(object sender, EventArgs e)
+        {
+            AbrirpagamentosESelecionarRadioButton(2);
+        }
+
+        private void btnBasico_Click(object sender, EventArgs e)
+        {
+            AbrirpagamentosESelecionarRadioButton(1);
+        }
+
+        private void btnPremium_Click(object sender, EventArgs e)
+        {
+            AbrirpagamentosESelecionarRadioButton(3);
+        }
+        private void AbrirForm2ESelecionarRadioButton(int radioButtonIndex)
+        {
+            Form pagamentos = new pagamentos();
+            pagamentos.SelecionarRadioButton(radioButtonIndex);
+            pagamentos.Show();
+        }
+    }
+}
